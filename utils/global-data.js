2055a54d68edfc4247856314ad7a3db3dd092e32
lixiny93 <79346384+lixiny93@users.noreply.github.com>
2022-05-07 11:15:38 +0800

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'TianriBlog';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'TianriBlog';
+    : '永远相信美好的事情即将发生';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'Always believe that something good is about to happen.';
